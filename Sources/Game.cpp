9b523aff02ca06738f46284357b27c24e3f7f5b1
hw16a178 <hw16a178@oecu.jp>
2017-11-14 16:47:27 +0900

    雲の移動と砲台の移動を編集しました。

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -2,8 +2,8 @@
 
 
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)　HW16A194 峯岡
-// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
-// TODO: 砲台を青い壁に沿って上下に動かす。(C)
+// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)　HW16A178　前田
+// TODO: 砲台を青い壁に沿って上下に動かす。(C)　HW16A178　前田
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D) Hw16A190 水本浩暉
 // TODO: スコアのサイズを大きくする。(E)　HW16A190 水本浩暉
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)　HW16A194 峯岡
@@ -19,6 +19,7 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
+int     flag=0;
 
 
 // ゲーム開始時に呼ばれる関数です。
@@ -36,6 +37,12 @@ void Start()
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
 void Update()
 {
+    //雲の移動
+    if (cloudPos.x < 310) {
+        cloudPos.x += 50 * Time::deltaTime;
+    }else{
+        cloudPos.x =-550;
+    }
     // 弾の発射
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
@@ -54,6 +61,20 @@ void Update()
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
             PlaySound("se_maoudamashii_system20.mp3");
         }
+    }
+
+    //砲台の移動
+    if(cannonPos.y < -75&&(flag==0||flag==2)){
+        flag=0;
+    }else if(cannonPos.y>-85&&(flag==0||flag==2)){
+        flag=1;
+    }else if(cannonPos.y<-145){
+        flag=2;
+    }
+    if (flag==0||flag==2){
+        cannonPos.y += 50 * Time::deltaTime;
+    }else{
+        cannonPos.y -= 50 * Time::deltaTime;
     }
 
     // 背景の描画
